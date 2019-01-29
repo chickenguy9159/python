257ce8f07503e6a138e240d92a0e99ae4c09e0b0
chickenguy9159 <44188814+chickenguy9159@users.noreply.github.com>
2019-01-29 18:02:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+My Homework Assignment 1
+"""
+#One Love
+
+Bside="get ready"
+Released="21 October 2002"
+Format="CD single"+"Compact Cassette"
+Recorded=2002
+Genre="R&B"+"pop"
+Length="3"+":"+"25"
+Label="Inocent"+"Virgin"
+Songwriters="Mikkel S. Eriksen"+" Tor Erik Hermansen"+" Hallgeir Rustan"+" Lee Ryan"+" Simon Webbe"+" Duncan James"+" Antony Costa"
+Producer="Stargate"
+print(Bside)
+print(Released)
+print(Format)
+print(Recorded)
+print(Genre)
+print(Length)
+print(Label)
+print(Songwriters)
+print(Producer)
